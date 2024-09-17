9401a702b43f684990ed09f47ef0b289bb9e5c19
kniskiukas <kniskiukas@gmail.com>
2024-09-17 18:29:19 +0300

    BNF and Lib1 changes

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,32 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = [
+    -- actions
+    "addCar",
+    "removeCar",
+    "listCars",
+    "findCar",
+    "updateCar",
+    "quit",
+    -- colors
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "black",
+    "white",
+    -- fuel types
+    "gasoline",
+    "diesel",
+    "electric",
+    "hybrid",
+    -- models
+    "sedan",
+    "coupe",
+    "convertible",
+    "hatchback",
+    "suv",
+    "truck",
+    "van"
+    ]
