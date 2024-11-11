4dcda8e0ca426b14235e4a8dd8aeead4e33b3252
Augustas Kniška <kniskiukas@gmail.com>
2024-11-11 12:01:31 +0200

    applicative and functor requirement

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -2,6 +2,7 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds -Wname-shadowing #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant lambda" #-}
+{-# LANGUAGE InstanceSigs #-}
 
 
 module Lib2
@@ -20,26 +21,37 @@ module Lib2
     ) where
 
 import qualified Data.Char as C
-import qualified Data.List as L
+import Control.Applicative
 
-type Parser a = String -> Either String (a, String)
+
+
+-- type Parser a = String -> Either String (a, String)
+
+newtype Parser a = Parser {
+    runParser :: String -> Either String (a, String)
+}
 
 parseItem :: Parser Items
-parseItem s = case many parseAlphaNum s of
-  Left e -> Left e
-  Right (cs, r) -> Right (Items [cs], r)
+-- parseItem s = case many parseAlphaNum s of
+--   Left e -> Left e
+--   Right (cs, r) -> Right (Items [cs], r)
+parseItem = Items . (:[]) <$> parseAlphaNumString
+
 
 parseItems :: Parser Items
-parseItems = or2  (and3 (\(Items i) _ (Items it) -> Items (head i : it)) parseItem parseComma parseItems) parseItem
+-- parseItems = or2  (and3 (\(Items i) _ (Items it) -> Items (head i : it)) parseItem parseComma parseItems) parseItem
+parseItems = Items <$> many (parseAlphaNumString <* parseComma)
 
 parseRequestId :: Parser Int
-parseRequestId input =
-  case many parseDigit input of
-    Left err -> Left err
-    Right (digits, rest) -> Right (read digits, rest)
+-- parseRequestId input =
+--   case many parseDigit input of
+--     Left err -> Left err
+--     Right (digits, rest) -> Right (read digits, rest)
+parseRequestId = read <$> many parseDigit
 
 parseRequest :: Parser Request
-parseRequest = and7 (\n _ t _ o _ i -> Request n t o i) parseRequestId parseComma (many parseLetter) parseComma (many parseLetter) parseComma parseItems
+-- parseRequest = and7 (\n _ t _ o _ i -> Request n t o i) parseRequestId parseComma (many parseLetter) parseComma (many parseLetter) parseComma parseItems
+parseRequest = Request <$> (parseRequestId <* parseComma) <*> (many parseLetter <* parseComma) <*> (many parseLetter <* parseComma) <*> parseItems
 
 data Query =
   AddRequest Request
@@ -62,32 +74,44 @@ newtype Items = Items [String]
   deriving (Eq, Show)
 
 parseQuery :: String -> Either String Query
-parseQuery s = case or2 parseAddRequest (or2 parseListRequests (or2 parseRemoveRequest (or2 parseUpdateRequest (or2 parseFindRequest parseRemoveAllRequests))))
-  s of
-    Left _ -> Left ("Unexpected command: " ++ s)
-    Right (v, r) -> if null r then Right v else Left ("Unexpected '" ++ r ++ "' after parsing")
+-- parseQuery s = case or2 parseAddRequest (or2 parseListRequests (or2 parseRemoveRequest (or2 parseUpdateRequest (or2 parseFindRequest parseRemoveAllRequests))))
+--   s of
+--     Left _ -> Left ("Unexpected command: " ++ s)
+--     Right (v, r) -> if null r then Right v else Left ("Unexpected '" ++ r ++ "' after parsing")
+parseQuery s = case runParser (parseAddRequest <|> parseListRequests <|> parseRemoveRequest <|> parseUpdateRequest <|> parseFindRequest <|> parseRemoveAllRequests) s of
+  Left err -> Left err
+  Right (result, rest) -> if null rest then Right result else Left ("Unexpected '" ++ rest ++ "' after parsing")
 
 parseAddRequest :: Parser Query
-parseAddRequest = and3 (\_ _ r -> AddRequest r) (parseString "add_request") parseSpace parseRequest
+-- parseAddRequest = and3 (\_ _ r -> AddRequest r) (parseString "add_request") parseSpace parseRequest
+
+parseAddRequest =  AddRequest <$> (parseString "add_request" *> parseSpace *> parseRequest)
 
 parseListRequests :: Parser Query
-parseListRequests s = case parseString "list_requests" s of
-  Left err -> Left err
-  Right (_, r) -> Right (ListRequests, r)
+-- parseListRequests s = case parseString "list_requests" s of
+--   Left err -> Left err
+--   Right (_, r) -> Right (ListRequests, r)
+
+parseListRequests = ListRequests <$ parseString "list_requests"
 
 parseRemoveRequest :: Parser Query
-parseRemoveRequest = and3 (\_ _ n -> RemoveRequest n) (parseString "remove_request") parseSpace parseRequestId
+parseRemoveRequest = RemoveRequest <$> (parseString "remove_request" *> parseSpace *> parseRequestId)
+
 
 parseUpdateRequest :: Parser Query
-parseUpdateRequest = and5 (\_ _ n _ r -> UpdateRequest n r) (parseString "update_request") parseSpace parseRequestId parseSpace parseRequest
+-- parseUpdateRequest = and5 (\_ _ n _ r -> UpdateRequest n r) (parseString "update_request") parseSpace parseRequestId parseSpace parseRequest
+parseUpdateRequest = UpdateRequest <$> (parseString "update_request" *> parseSpace *> parseRequestId) <*> (parseSpace *> parseRequest)
 
 parseFindRequest :: Parser Query
-parseFindRequest = and3 (\_ _ n -> FindRequest n) (parseString "find_request") parseSpace parseRequestId
+-- parseFindRequest = and3 (\_ _ n -> FindRequest n) (parseString "find_request") parseSpace parseRequestId
+parseFindRequest = FindRequest <$> (parseString "find_request" *> parseSpace *> parseRequestId)
 
 parseRemoveAllRequests :: Parser Query
-parseRemoveAllRequests s = case parseString "remove_all_requests" s of
-  Left err -> Left err
-  Right (_, r) -> Right (RemoveAllRequests, r)
+-- parseRemoveAllRequests s = case parseString "remove_all_requests" s of
+--   Left err -> Left err
+--   Right (_, r) -> Right (RemoveAllRequests, r)
+
+parseRemoveAllRequests = RemoveAllRequests <$ parseString "remove_all_requests"
 
 newtype State =
   State {
@@ -111,9 +135,9 @@ stateTransition s (RemoveRequest i) =
     then Left "Request does not exist"
     else Right (Just "Request removed", State (filter (\r -> requestId r /= i) (requests s)))
 
-stateTransition s (RemoveAllRequests) = 
-  if null (requests s) 
-    then Left "No requests to remove" 
+stateTransition s RemoveAllRequests =
+  if null (requests s)
+    then Left "No requests to remove"
     else Right (Just "All requests removed", State [])
 
 stateTransition s (UpdateRequest i r) =
@@ -132,8 +156,9 @@ requestExists r s = r `elem` requests s
 --basic parsers
 
 parseChar :: Char -> Parser Char
-parseChar _ [] = Left "Cannot find character in an empty input"
-parseChar c s@(h : t) = if c == h then Right (c, t) else Left ("Expected '" ++ [c] ++ "' but found '" ++ [h] ++ "' in " ++ s)
+parseChar c = Parser $ \input -> case input of
+  [] -> Left ("Cannot find " ++ [c] ++ " in an empty input")
+  (h : t) -> if h == c then Right (c, t) else Left (input ++ " does not start with " ++ [c])
 
 parseSpace :: Parser Char
 parseSpace = parseChar ' '
@@ -142,118 +167,73 @@ parseComma :: Parser Char
 parseComma = parseChar ','
 
 parseDigit :: Parser Char
-parseDigit [] = Left "Cannot find any digits in an empty input"
-parseDigit s@(h : t) = if C.isDigit h then Right (h, t) else Left (s ++ " does not start with a digit")
+-- parseDigit [] = Left "Cannot find any digits in an empty input"
+-- parseDigit s@(h : t) = if C.isDigit h then Right (h, t) else Left (s ++ " does not start with a digit")
+parseDigit = Parser $ \input -> case input of
+  [] -> Left "Cannot find any digits in an empty input"
+  (h : t) -> if C.isDigit h then Right (h, t) else Left (input ++ " does not start with a digit")
 
 parseLetter :: Parser Char
-parseLetter [] = Left "Cannot find any letter in an empty input"
-parseLetter s@(h:t) = if C.isLetter h then Right (h, t) else Left (s ++ " does not start with a letter")
+parseLetter = Parser $ \input -> case input of
+  [] -> Left "Cannot find any letter in an empty input"
+  (h : t) -> if C.isLetter h then Right (h, t) else Left (input ++ " does not start with a letter")
 
 parseAlphaNum :: Parser Char
-parseAlphaNum [] = Left "Cannot find any alphanumeric character in an empty input"
-parseAlphaNum s@(h:t) = if C.isAlphaNum h then Right (h, t) else Left (s ++ " does not start with an alphanumeric character")
+-- parseAlphaNum [] = Left "Cannot find any alphanumeric character in an empty input"
+-- parseAlphaNum s@(h:t) = if C.isAlphaNum h then Right (h, t) else Left (s ++ " does not start with an alphanumeric character")
+parseAlphaNum = parseLetter <|> parseDigit
 
 parseString :: String -> Parser String
-parseString [] s = Right ([], s)
-parseString (c:cs) s = case parseChar c s of
-  Left err -> Left err
-  Right (_, rest) -> case parseString cs rest of
-    Left err -> Left err
-    Right (v2, r2) -> Right (c:v2, r2)
+parseString = foldr (\ h -> (<*>) ((:) <$> parseChar h)) (pure [])
+-- parseString [] = pure []
+-- parseString (h : t) = (:) <$> parseChar h <*> parseString t
+
+parseAlphaNumString :: Parser String
+parseAlphaNumString = many parseAlphaNum
+
+
 
 parseManyLetters :: Parser String
-parseManyLetters = many1 parseLetter
+parseManyLetters = many parseLetter
 
-parseLetter :: Parser Char
-parseLetter [] = Left "Cannot find any letter in an empty input"
+-- needed helpers
+-- class Functor f where
+--   fmap :: (a -> b) -> f a -> f b
+--   pure :: a -> f a
+--   (<*>) :: f (a -> b) -> f a -> f b
+--   (>>=) :: f a -> (a -> f b) -> f b
+--   (>>) :: f a -> f b -> f b
 
-parseLetter s@(h:t) = if C.isLetter h then Right (h, t) else Left (s ++ " does not start with a letter")
-
-
---helper parsers
-
-many :: Parser a -> Parser [a]
-many p = many' p []
-    where
-        many' p' acc = \input ->
-            case p' input of
-                Left _ -> Right (acc, input)
-                Right (v, r) -> many' p' (acc ++ [v]) r
-
-or2 :: Parser a -> Parser a -> Parser a
-or2 a b = \input ->
-    case a input of
-        Right r1 -> Right r1
-        Left e1 ->
-            case b input of
-                Right r2 -> Right r2
-                Left e2 -> Left (e1 ++ ", " ++ e2)
-
-and2 :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-and2 f p1 p2 s = case p1 s of
-  Left err -> Left err
-  Right (v1, r1) -> case p2 r1 of
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f p = Parser $ \s -> case runParser p s of
     Left err -> Left err
-    Right (v2, r2) -> Right (f v1 v2, r2)
+    Right (result, rest) -> Right (f result, rest)
 
-and3 :: (a -> b -> c -> d) -> Parser a -> Parser b -> Parser c -> Parser d
-and3 f p1 p2 p3 s = case p1 s of
-  Left err -> Left err
-  Right (v1, r1) -> case p2 r1 of
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure x = Parser $ \s -> Right (x, s)
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  pf <*> px = Parser $ \s -> case runParser pf s of
     Left err -> Left err
-    Right (v2, r2) -> case p3 r2 of
+    Right (f, rest) -> case runParser px rest of
       Left err -> Left err
-      Right (v3, r3) -> Right (f v1 v2 v3, r3)
+      Right (x, rest') -> Right (f x, rest')
 
-and4 :: (a -> b -> c -> d -> e) -> Parser a -> Parser b -> Parser c -> Parser d -> Parser e
-and4 f p1 p2 p3 p4 s = case p1 s of
-  Left err -> Left err
-  Right (v1, r1) -> case p2 r1 of
-    Left err -> Left err
-    Right (v2, r2) -> case p3 r2 of
-      Left err -> Left err
-      Right (v3, r3) -> case p4 r3 of
-        Left err -> Left err
-        Right (v4, r4) -> Right (f v1 v2 v3 v4, r4)
+instance Alternative Parser where
+  empty :: Parser a
+  empty = Parser $ \_ -> Left "empty"
+  (<|>) :: Parser a -> Parser a -> Parser a
+  a <|> b = Parser $ \s -> case runParser a s of
+    Left _ -> runParser b s
+    Right r -> Right r
+  -- many :: Parser a -> Parser [a]
+  -- many p = many' p []
+  --   where
+  --     many' p' acc = \input ->
+  --       case p' input of
+  --         Left _ -> Right (acc, input)
+  --         Right (v, r) -> many' p' (acc ++ [v]) r
 
-and5 :: (a -> b -> c -> d -> e -> f) -> Parser a -> Parser b -> Parser c -> Parser d -> Parser e -> Parser f
-and5 f p1 p2 p3 p4 p5 s = case p1 s of
-  Left err -> Left err
-  Right (v1, r1) -> case p2 r1 of
-    Left err -> Left err
-    Right (v2, r2) -> case p3 r2 of
-      Left err -> Left err
-      Right (v3, r3) -> case p4 r3 of
-        Left err -> Left err
-        Right (v4, r4) -> case p5 r4 of
-          Left err -> Left err
-          Right (v5, r5) -> Right (f v1 v2 v3 v4 v5, r5)
-
-and7 :: (a -> b -> c -> d -> e -> f -> g -> h) -> Parser a -> Parser b -> Parser c -> Parser d -> Parser e -> Parser f -> Parser g -> Parser h
-and7 f p1 p2 p3 p4 p5 p6 p7 s = case p1 s of
-  Left err -> Left err
-  Right (v1, r1) -> case p2 r1 of
-    Left err -> Left err
-    Right (v2, r2) -> case p3 r2 of
-      Left err -> Left err
-      Right (v3, r3) -> case p4 r3 of
-        Left err -> Left err
-        Right (v4, r4) -> case p5 r4 of
-          Left err -> Left err
-          Right (v5, r5) -> case p6 r5 of
-            Left err -> Left err
-            Right (v6, r6) -> case p7 r6 of
-              Left err -> Left err
-              Right (v7, r7) -> Right (f v1 v2 v3 v4 v5 v6 v7, r7)
-
-many1 :: Parser a -> Parser [a]
-many1 p s = case p s of
-  Left err -> Left err
-  Right (v1, r1) -> case many1' r1 of
-    (v2, r2) -> Right (v1:v2, r2)
-  where
-    many1' s2 = case p s2 of
-      Left _ -> ([], s2)
-      Right (v2, r2) -> let (vs, r3) = many1' r2 in (v2 : vs, r3)
 
 
